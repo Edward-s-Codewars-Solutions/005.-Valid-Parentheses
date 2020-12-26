507970ed0307087f0f3aebb478779f8982c4e2c9
Edward Zieminski <Edward.Zieminski@gmail.com>
2020-12-26 18:08:23 +0100

    Implemented high level method

--- KataLibrary/Parentheses.cs
+++ KataLibrary/Parentheses.cs
@@ -2,8 +2,23 @@
 
 public static class Parentheses
 {
+    private const int MaxInputLength = 100;
+
     public static bool ValidParentheses(string input)
     {
+        return input switch
+        {
+            var inp
+                when inp.Length > MaxInputLength
+                    => false,
+            var inp => DoesEachParenHaveItsPair(inp)
+        };
+    }
+
+    private static bool DoesEachParenHaveItsPair(string input)
+    {
+
+
         throw new NotImplementedException();
     }
 }
