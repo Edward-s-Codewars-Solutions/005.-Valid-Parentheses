2c95916944f9b023dfea2416deeebecc4d56a630
Edward Zieminski <Edward.Zieminski@gmail.com>
2020-12-26 18:47:18 +0100

    Prepared UT for method before implementation

---
+++ UnitTests/MyUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+public class MyUnitTests
+{
+    private static IEnumerable<TestCaseData> TestDataWitParens
+    {
+        get
+        {
+            yield return new TestCaseData(")(").Returns(false);
+            yield return new TestCaseData("(").Returns(false);
+            yield return new TestCaseData("(()").Returns(false);
+            yield return new TestCaseData(")").Returns(false);
+            yield return new TestCaseData("").Returns(true);
+            yield return new TestCaseData("()").Returns(true);
+            yield return new TestCaseData("()()").Returns(true);
+            yield return new TestCaseData("(())").Returns(true);
+            yield return new TestCaseData("(abcd)(@#%$)").Returns(true);
+        }
+    }
+
+    [Test, TestCaseSource("TestDataWithParens")]
+    public bool Are_parens_paired_correctly(string input) =>
+        Parentheses.DoesEachParenHaveItsPair(input);
+}
